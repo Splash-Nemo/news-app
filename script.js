f9c922886176bf8984853774587da968a7068029
Aditya <ap457codes@gmail.com>
2024-05-25 08:51:28 +0530

    next button onClick event done headlines,adn short-headlines

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 let headlineItems = document.querySelectorAll('.headline-item'); // Select all headline items
+let shortHeadlineItems = document.querySelectorAll('.short-headline-item');
 let nextButton = document.querySelector('.next-button');
 
 let currentIndex = 0; // Track the index of the current active element
+let pointer = 1;
 
 nextButton.addEventListener('click', () => {
-    if(currentIndex>0){
-        let abc = currentIndex-1;
+    // Handling for the main headline items
+    if (currentIndex > 0) {
+        let abc = currentIndex - 1;
         headlineItems[abc].classList.remove('next');
-        headlineItems[abc].style.zIndex='99';
-        headlineItems[abc].style.transform="translateX(0)";
-    }else if(currentIndex===0){
-        let abc=headlineItems.length-1;
+        headlineItems[abc].style.zIndex = '99';
+        headlineItems[abc].style.transform = "translateX(0)";
+    } else if (currentIndex === 0) {
+        let abc = headlineItems.length - 1;
         headlineItems[abc].classList.remove('next');
-        headlineItems[abc].style.zIndex='99';
-        headlineItems[abc].style.transform="translateX(0)";
+        headlineItems[abc].style.zIndex = '99';
+        headlineItems[abc].style.transform = "translateX(0)";
     }
+
     // Rearrange the headline items based on the current index
     let container = document.querySelector('.headlines-list');
     headlineItems[currentIndex].classList.add('next');
-    
     container.appendChild(headlineItems[currentIndex]);
+
     // Increment currentIndex, and if it exceeds the length of headlineItems, loop back to 0
     currentIndex = (currentIndex + 1) % headlineItems.length;
-    headlineItems[currentIndex].style.zIndex='100';
+    headlineItems[currentIndex].style.zIndex = '100';
+
+    // Handling for the short headline items
+    let current = shortHeadlineItems[pointer];
+    let beforeIndex = pointer - 1;
+
+    current.style.backgroundColor = 'red';
+    if (beforeIndex < 0)
+        shortHeadlineItems[shortHeadlineItems.length - 1].style.backgroundColor = '#22282E';
+    else
+        shortHeadlineItems[beforeIndex].style.backgroundColor = '#22282E';
+    pointer = (pointer + 1) % shortHeadlineItems.length;
 });
 
 
